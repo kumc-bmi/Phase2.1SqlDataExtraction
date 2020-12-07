680b483603824ae5eb9715c4d3aa1849921ab607
Lav Patel <contact.lavpatel@gmail.com>
2020-12-07 10:52:29 -0600

    trying to remove blank spaces

--- export/2.1/P2PATIENTCLINICALCOURSE.sql
+++ export/2.1/P2PATIENTCLINICALCOURSE.sql
@@ -1,7 +1,10 @@
 ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
-set linesize 32000
+SET TRIMSPOOL ON; -- otherwise every line in the spoolfile is filled up with blanks until the linesize is reached.
+SET TRIMOUT ON; -- otherwise every line in the output is filled up with blanks until the linesize is reached.
+SET WRAP OFF; -- Truncates the line if its is longer then LINESIZE. This should not happen if linesize is large enough.
+SET TERMOUT OFF; -- suppresses the printing of the results to the output. The lines are still written to the spool file. This may accelerate the exectution time of a statement a lot.
+set linesize 32000;
 set pagesize 0  -- No header rows
-set trimspool on -- remove trailing blanks
 set feedback off
 set markup csv on
 spool LocalPatientCLinicalCourse.csv
